04096d4c3f545933089a508626e2ef4581d63700
Khagendra1814138 <75677824+Khagendra1814138@users.noreply.github.com>
2023-06-15 15:07:36 +0100

    Wireframe

--- src/App.js
+++ src/App.js
@@ -1,9 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
+import './styles/NavBar.css';
 
 function App() {
   return (
     <div className="App">
+      <nav className='navFrame'>
+        <logo className="siteLogo">Logo</logo>
+
+        <ul className='Navlists'>
+          <li>New Arrivals</li>
+          <li>Mens</li>
+          <li>Womens</li>
+          <li>Kids</li>
+        </ul>
+
+      </nav>
+
+      <header className="header"> Header section  
+
+      </header>
+
+      <section className='TrendingSection'>
+        <div className='trendingInfo'>Trending Products This Week!
+          <button>Shop</button>
+        </div>
+        <div className='trendingProductsFrame'>Trending Carousel Style</div>
+      </section>
+
+      <h1>Who are you shoipping for?</h1>
+      <section className='whoForSection'>
+        <div className='whoForChoicesFrame'>Men</div>
+        <div className='whoForChoicesFrame'>Women</div>
+        <div className='whoForChoicesFrame'>Kid</div>
+      </section>
+
+
+      <h1>Shop By Sport</h1>
+      <section className='whoForSection'>
+        <div>Football</div>
+        <div>Basketball</div>
+        <div>Tennis</div>
+        <div>Running</div>
+        <div>Skateboard</div>
+        <div>Training</div>
+        <div>Yoga</div>
+        <div>Dance</div>
+      </section>
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -22,16 +66,6 @@ function App() {
             <div className="coverBL"></div>
             <div className="coverBR"></div>
         </div>
-
-        
-        <div class="wrapper">
-            <header className="things">
-                <image class="background">Background</image>
-                <image class="foreground">Foreground</image>
-                <h class="title">Welcome!</h>
-            </header>
-        </div>
-
       </header>
     </div>
   );
