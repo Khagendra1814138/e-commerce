a7f40d793abf6aaa7267a755812a2626264a391b
Khagendra1814138 <75677824+Khagendra1814138@users.noreply.github.com>
2023-06-22 00:10:12 +0100

    Small

--- src/pages/KidSection/Kids.js
+++ src/pages/KidSection/Kids.js
@@ -1,10 +1,50 @@
 import React from "react";
 
 import "../../styles/KidsPage.css";
+import "../../styles/SideFilter.css";
+import "../../styles/ProductCard.css";
 
 function KidsPage() {
     return (
-      <div className="KidsPage">Kids Section
+      <div className="KidsPage">
+
+        <h1>KIDS FOOTWARE</h1>
+
+        <section className="productDisplayFrame">
+          <div className="sideFilterFrame"> Side Filter
+            <box className="filterOptions"> Gender </box>
+            <box className="filterOptions"> Colors </box>
+            <box className="filterOptions"> Size </box>
+            <box className="filterOptions"> Sport </box>
+            <box className="filterOptions"> Sales & Deal </box>
+          </div>
+          <div className="ProductDisplay"> 
+            
+            <card className="productCard">
+              <image></image>
+              <detail className="detailFrame">
+
+                <div className="productDetail">
+                  <div className="productName">Air Force 1</div>
+                  <div className="productPrice">£90.00</div>
+                </div>
+              </detail>
+              <button className="addToBag">Add to Bag</button>
+            </card>
+
+            <card className="productCard">2</card>
+            <card className="productCard">3</card>
+            <card className="productCard">4</card>
+            <card className="productCard">5</card>
+            <card className="productCard">6</card>
+            <card className="productCard">7</card>
+            <card className="productCard">8</card>
+            <card className="productCard">9</card>
+            <card className="productCard">10</card>
+            <card className="productCard">11</card>
+            <card className="productCard">12</card>
+          </div>
+        </section>
   
       </div>
     );
--- src/pages/MenSection/Men.js
+++ src/pages/MenSection/Men.js
@@ -1,16 +1,18 @@
 import React from "react";
 
 import "../../styles/MensPage.css";
+import "../../styles/SideFilter.css";
+import "../../styles/ProductCard.css";
 
 
 function MensPage() {
     return (
       <div className="MenPage">
         
-        <header className="mensHeader">
+        {/* <header className="mensHeader">
           <h1>MENS FOOTWARE</h1>
           <image className="focusedProduct">Product Image</image>
-        </header>
+        </header> */}
 
         <section className="trendingThisWeekSection">
           <h1>Trending This Week</h1>
@@ -26,29 +28,42 @@ function MensPage() {
             </div>
         </section>
 
-        <section className="trendingThisWeekSection">
-          <h1>Latest Releases</h1>
-          <div className='mensTrendingProductsFrame'>
-            <div className='mensTrendingCards'>1</div>
-            <div className='mensTrendingCards'>2</div>
-            <div className='mensTrendingCards'>3</div>
-            <div className='mensTrendingCards'>4</div>
-            <div className='mensTrendingCards'>5</div>
-            <div className='mensTrendingCards'>6</div>
-            <div className='mensTrendingCards'>7</div>
-            <div className='mensTrendingCards'>8</div>
-          </div>
-        </section>
+        <h1>MENS FOOTWARE</h1>
 
         <section className="productDisplayFrame">
-            <div className='mensTrendingCards'>1</div>
-            <div className='mensTrendingCards'>2</div>
-            <div className='mensTrendingCards'>3</div>
-            <div className='mensTrendingCards'>4</div>
-            <div className='mensTrendingCards'>5</div>
-            <div className='mensTrendingCards'>6</div>
-            <div className='mensTrendingCards'>7</div>
-            <div className='mensTrendingCards'>8</div>
+          <div className="sideFilterFrame"> Side Filter
+            <box className="filterOptions"> Gender </box>
+            <box className="filterOptions"> Colors </box>
+            <box className="filterOptions"> Size </box>
+            <box className="filterOptions"> Sport </box>
+            <box className="filterOptions"> Sales & Deal </box>
+          </div>
+
+          <div className="ProductDisplay"> 
+            <card className="productCard">
+              <image></image>
+              <detail className="detailFrame">
+
+                <div className="productDetail">
+                  <div className="productName">Air Force 1</div>
+                  <div className="productPrice">£90.00</div>
+                </div>
+              </detail>
+              <button className="addToBag">Add to Bag</button>
+            </card>
+
+            <card className="productCard">2</card>
+            <card className="productCard">3</card>
+            <card className="productCard">4</card>
+            <card className="productCard">5</card>
+            <card className="productCard">6</card>
+            <card className="productCard">7</card>
+            <card className="productCard">8</card>
+            <card className="productCard">9</card>
+            <card className="productCard">10</card>
+            <card className="productCard">11</card>
+            <card className="productCard">12</card>
+          </div>
         </section>
 
 
