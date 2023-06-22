473d2bc7f2dfedfa16dfa0e4eed3327dccde099c
Khagendra1814138 <75677824+Khagendra1814138@users.noreply.github.com>
2023-06-23 00:04:48 +0100

    Update NewArrivals.js

--- src/pages/NewSection/NewArrivals.js
+++ src/pages/NewSection/NewArrivals.js
@@ -2,14 +2,14 @@ import React from "react";
 
 import "../../styles/NewArrivals.css";
 
-import Carousel from "../../components/carousel/Carousel";
-import { Shoes } from "../../components/carousel/Items";
+// import Carousel from "../../components/carousel/Carousel";
+// import { Shoes } from "../../components/carousel/Items";
 
 function NewArrivals() {
     return (
       <div className="NewArrivals">New Arrivals Section
 
-        <Carousel images={Shoes} />
+        {/* <Carousel images={Shoes} /> */}
   
       </div>
     );
